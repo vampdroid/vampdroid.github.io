44a0b684d09add63a11fa7eda666a3fadc4f7648
Yash Kukreja <yashskukreja@gmail.com>
2026-02-21 19:12:37 +0530

    Remove lottie and keep normal svg

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
-import lottie from "astro-integration-lottie";
 
 export default defineConfig({
     site: 'https://yashkukreja.com',
-    integrations: [mdx(), sitemap(), react(), lottie(),],
+    integrations: [mdx(), sitemap(), react()],
 });
