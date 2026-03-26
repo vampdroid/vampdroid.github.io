c5c19b749652b5599ba713a14aa992fc1c673db7
Yash Kukreja <yashskukreja@gmail.com>
2026-03-26 20:40:24 +0530

    Improve CWV

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Yash Kukreja';
-export const SITE_DESCRIPTION = 'I love creating solutions!';
+export const SITE_TITLE = 'Yash Kukreja | Software Engineer & Content Creator';
+export const SITE_DESCRIPTION = 'Explore the portfolio of Yash Kukreja, a Software Engineer and Content Creator focused on building modern web applications and sharing tech knowledge.';
